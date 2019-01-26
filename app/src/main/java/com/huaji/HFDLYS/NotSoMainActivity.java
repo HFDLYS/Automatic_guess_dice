e9138e2fdc9a48bee404a11bd0e5b916cef576fb
HFDLYS <845388925@qq.com>
2019-01-26 12:40:02 +0800

    ok

--- app/src/main/java/com/huaji/HFDLYS/NotSoMainActivity.java
+++ app/src/main/java/com/huaji/HFDLYS/NotSoMainActivity.java
@@ -19,9 +19,38 @@ public class NotSoMainActivity extends Activity
 			@Override
 			public void onClick(View v)
 			{
-				Toast.makeText(NotSoMainActivity.this,"我出千了，你输了",Toast.LENGTH_LONG).show();
+				
+				Toast.makeText(NotSoMainActivity.this,"你输入的是",Toast.LENGTH_LONG).show();
 			}
 		});
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.alittlemain , menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		{
+			switch (item.getItemId())
+			{
+				case R.id.QQ:
+					Toast.makeText(this , "作者QQ845388925" ,Toast.LENGTH_SHORT).show();
+					break;
+				case R.id.leave:
+					Toast.makeText(this , "爱玩玩不玩滚" ,Toast.LENGTH_SHORT).show();
+					finish();
+					break;
+				default:
+			}
+			return true;
+		}
 	}
 }
 
